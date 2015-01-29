422acee2c371f6b056a8673d7883c0170a35b102
fanyou <fanyou>
2015-01-29 00:50:29 +0000

    merger

--- 15440-p1/work/server.c
+++ 15440-p1/work/server.c
@@ -52,8 +52,9 @@ int main(int argc, char**argv) {
 		// get messages and send replies to this client, until it goes away
 		while ( (rv=recv(sessfd, buf, MAXMSGLEN, 0)) > 0) {
 			buf[rv]=0;		// null terminate string to print
-			if(buf == "open") {
-				printf("is openg");
+			printf("%s", buf);
+			if(strcmp(buf, "open\n") == 0) {
+				//get open , use open
 			}
 		}
 
