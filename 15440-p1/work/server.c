63458c30157a05c33431f19cfedb233836cdad1c
fan you <youfan.noey@gmail.com>
2015-01-30 02:16:34 -0500

    first attemp

--- 15440-p1/work/server.c
+++ 15440-p1/work/server.c
@@ -10,7 +10,7 @@
 #include <err.h>
 #include <stdint.h>
 
-#define MAXMSGLEN 100
+#define MAXMSGLEN 1000
 
 enum system_call {
 	OPEN,
@@ -24,21 +24,25 @@ int read_int32(int sessfd) {
 	char buf[MAXMSGLEN+1];
 	if((rv = recv(sessfd, buf, MAXMSGLEN, 0)) > 0) {
 		buf[rv] = 0;
-		printf("%d", buf[0] - '0');
+		//printf("%d", buf[0] - '0');
 		//return (int)(buf - '0');
 	}
-		return buf[0] - '0';
+	return buf[0] - '0';
 }
 
-const char* read_string(int sessfd) {
-	int rv, length;
-	char* buf = malloc(MAXMSGLEN+1);
-	//char* str = malloc();
+char* read_string(int sessfd) {
+	int rv;
+	char buf[MAXMSGLEN+1];
 	if((rv = recv(sessfd, buf, MAXMSGLEN, 0)) > 0) {
-		buf[rv] = 0;
+		char* recv_str = malloc(rv+1);
+		for(int i = 0; i < rv; i++) {
+			recv_str[i] = buf[i];
+		}
+		recv_str[rv] = 0;
 		//length = buf[0]; //length of path
+		return recv_str;
 	}
-	return buf;
+		return NULL;
 }
 
 int main(int argc, char**argv) {
@@ -48,16 +52,16 @@ int main(int argc, char**argv) {
 	int sockfd, sessfd, rv;
 	struct sockaddr_in srv, cli;
 	socklen_t sa_size;
-	
+
 	// Get environment variable indicating the port of the server
 	serverport = getenv("serverport15440");
 	if (serverport) port = (unsigned short)atoi(serverport);
 	else port=15440;
-	
+
 	// Create socket
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);	// TCP/IP socket
 	if (sockfd<0) err(1, 0);			// in case of error
-	
+
 	// setup address structure to indicate server port
 	memset(&srv, 0, sizeof(srv));			// clear it first
 	srv.sin_family = AF_INET;			// IP family
@@ -67,41 +71,60 @@ int main(int argc, char**argv) {
 	// bind to our port
 	rv = bind(sockfd, (struct sockaddr*)&srv, sizeof(struct sockaddr));
 	if (rv<0) err(1,0);
-	
+
 	// start listening for connections
 	rv = listen(sockfd, 5);
 	if (rv<0) err(1,0);
-	
+
 	// main server loop, handle clients one at a time, quit after 10 clients
-  while (1) {
-		
+	while (1) {
+
 		// wait for next client, get session socket
 		sa_size = sizeof(struct sockaddr_in);
 		sessfd = accept(sockfd, (struct sockaddr *)&cli, &sa_size);
 		if (sessfd<0) err(1,0);
-		
-//		// get messages and send replies to this client, until it goes away
-//		while ( (rv=recv(sessfd, buf, MAXMSGLEN, 0)) > 0) {
-//			buf[rv]=0;		// null terminate string to print
-//			if(strcmp(buf , "open")) {
-//				printf("is open!!");
-//			}
-//		}
-//=======
+
+		//		// get messages and send replies to this client, until it goes away
+		//		while ( (rv=recv(sessfd, buf, MAXMSGLEN, 0)) > 0) {
+		//			buf[rv]=0;		// null terminate string to print
+		//			if(strcmp(buf , "open")) {
+		//				printf("is open!!");
+		//			}
+		//		}
+
 		int fid = read_int32(sessfd);
 		if(fid == OPEN) {
 			const char* path = read_string(sessfd);
 			int flag = read_int32(sessfd);
 			open(path, flag);
-	}
+		}
 
-	// either client closed connection, or error
+		if(fid == CLOSE) {
+			int fd = read_int32(sessfd);
+			close(fd);
+		}
 
-	//if (rv==0) err(1,0);
-	//close(sessfd);
+		if(fid == WRITE) {
+			int fd = read_int32(sessfd);
+			char* buf = read_string(sessfd);
+			size_t nbyte = read_int32(sessfd);
+			write(fd, buf, nbyte);
+		}
 
-	if (rv<0) err(1,0);
-	close(sessfd);
+		if(fid == READ) {
+			int fd = read_int32(sessfd);
+			char* buf = read_string(sessfd);
+			size_t nbyte = read_int32(sessfd);
+			read(fd, buf, nbyte);
+		}
+
+		// either client closed connection, or error
+
+		//if (rv==0) err(1,0);
+		//close(sessfd);
+
+		if (rv<0) err(1,0);
+		close(sessfd);
 	}
 
 	// close socket
