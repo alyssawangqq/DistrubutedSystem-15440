e7006712e70466160ba721cd61cd3f9c57034be0
Your Name <you@example.com>
2015-02-18 11:56:43 -0500

    123

--- 15440-p2/work/Proxy.java
+++ 15440-p2/work/Proxy.java
@@ -46,8 +46,12 @@ class Proxy {
 					case CREATE_NEW:
 						System.err.println("CREATE_NEW");
 						//if(fs[fd].file.isDirectory()) return Errors.EISDIR;
-						fs[fd].raf = new RandomAccessFile(fs[fd].file, "rw");
-						if(fs[fd].file.exists()) return Errors.EEXIST;
+						//fs[fd].raf = new RandomAccessFile(fs[fd].file, "rw");
+						//if(fs[fd].file.exists()) return Errors.EEXIST; 
+						//Potential solution
+						if(fs[fd].file.exists()) {}//noth;
+						else 
+							fs[fd].raf = new RandomAccessFile(fs[fd].file, "rw");
 						break;
 					case READ:
 						System.err.println("READ");
@@ -64,6 +68,9 @@ class Proxy {
 				}			
 			}catch (FileNotFoundException e) {
 				e.printStackTrace();
+				System.err.println("!!!!!");
+				System.err.println("after stack trace");
+				System.err.println("!!!!!");
 				return Errors.ENOENT;
 				//return -1;
 			}
@@ -77,13 +84,15 @@ class Proxy {
 				if(fs[fd] != null) close(fd);
 				fd++;
 			}
-			return 1;
+			return 0;
 		}
 
 		public synchronized int close( int fd ) {
 			System.err.println("close called for fd" + fd);
 			if(fd >= 2048) {
 				fd -= 2048;
+			}else {
+				return Errors.EBADF;
 			}
 			if(fd < 0 || fs[fd] == null || fs[fd].raf == null || fs[fd].file == null) {System.err.println("close err"); return Errors.EBADF;}
 			try{
@@ -93,16 +102,20 @@ class Proxy {
 				fs[fd] = null;
 			}catch (IOException e) {
 				e.printStackTrace();
+				return -1;
 			}
-			return 1;
+			return 0;
 		}
 
 		public synchronized long write( int fd, byte[] buf ) {
 			System.err.println("write called for fd" + fd);
 			if(fd >= 2048) {
 				fd -= 2048;
+			}else {
+				return Errors.EBADF;
 			}
 			if(fd < 0 || fs[fd] == null || fs[fd].raf == null || fs[fd].file == null) {System.err.println("write err"); return Errors.EBADF;}
+			if(fs[fd].file.isDirectory()) return Errors.EISDIR;
 			try {
 				fs[fd].raf.write(buf);
 			}catch (IOException e) {
@@ -116,8 +129,11 @@ class Proxy {
 			System.err.println("read called for fd" + fd);
 			if(fd >= 2048) {
 				fd -= 2048;
+			}else {
+				return Errors.EBADF;
 			}
 			if(fd < 0 || fs[fd] == null || fs[fd].raf == null || fs[fd].file == null) {System.err.println("read err"); return Errors.EBADF;}
+			if(fs[fd].file.isDirectory()) return Errors.EISDIR;
 			try {
 				int ret = fs[fd].raf.read(buf);
 				if(ret == -1) {
@@ -135,8 +151,11 @@ class Proxy {
 			System.err.println("lseek called for fd" + fd);
 			if(fd >= 2048) {
 				fd -= 2048;
+			}else {
+				return Errors.EBADF;
 			}
 			if(fd < 0 || fs[fd] == null || fs[fd].raf == null || fs[fd].file == null) return Errors.EBADF;
+			if(fs[fd].file.isDirectory()) return Errors.EISDIR;
 			try{
 				switch(o) {
 					case FROM_CURRENT:
@@ -152,24 +171,25 @@ class Proxy {
 				}
 			}catch(IOException e) {
 				e.printStackTrace();
+				return -1;
 			}
-			return 1;
+			return 0;
 		}
 
 		public synchronized int unlink( String path ) {
 			System.err.println("unlink called for path" + path);
 			try {
 				File file = new File(path);
-				if(file.delete()) {
-					return 1;
-				}else {
-					if(!file.exists())  return Errors.ENOENT;
-					if(file.isDirectory() && file.list().length > 0) return Errors.ENOTEMPTY;
+				if(!file.exists())  return Errors.ENOENT;
+				if(file.isDirectory()) return Errors.EISDIR;
+				if(!file.delete()) {
+					return -1;
 				}
 			}catch(Exception e) {
 				e.printStackTrace();
+				return -1;
 			}
-			return Errors.EPERM;
+			return 0;
 		}
 
 		public void clientdone() {
