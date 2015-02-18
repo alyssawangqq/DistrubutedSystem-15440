fb042acf8fd00e53a36623d297381e2272db49fb
Your Name <you@example.com>
2015-02-18 00:11:49 -0500

    case

--- 15440-p2/work/case.c
+++ 15440-p2/work/case.c
@@ -8,12 +8,11 @@
 int main() {
 	char buf[1000] = "hhhhh";
 	int fd2 = open("b", O_CREAT | O_RDONLY, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH);
-	int ret, awrite;
 	printf("fd2 = %d\n", fd2);
 	write(fd2, buf, 6);
 	int _errno = errno;
-	printf("errno = %d\n", _errno);
-	perror(NULL);
+	//printf("errno = %d\n", _errno);
+	//perror(NULL);
 	close(fd2);
 	return 0;
 }
